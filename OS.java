af234894cfc445bfffcb97c3d08f1ff63d6e91c1
CreatorOfCryptids <dannypeck5000@gmail.com>
2024-02-29 23:17:33 -0500

    This is unstable as fuck, but im going to submit it.
    Will keep working to fix, but it's nice to have a semi working version in.

--- OS.java
+++ OS.java
@@ -283,6 +283,6 @@ public class OS {
      * @param message The message printed to the terminal.
      */
     public static void dbMes(String message){
-        System.out.println("    ||"+message);
+        //System.out.println("    ||"+message);
     }
 }

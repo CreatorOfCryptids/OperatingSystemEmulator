34a367feb7e56ee7f1e6bc285f21e487b84e8742
CreatorOfCryptids <dannypeck5000@gmail.com>
2024-04-17 20:25:25 -0400

    Make it loop less, so it doesn;t overfill my computer.

--- Piggy.java
+++ Piggy.java
@@ -14,16 +14,17 @@ public class Piggy extends UserLandProcess{
         int pointer = OS.allocateMemory(99 * UserLandProcess.PAGE_SIZE);
         byte[] oink = "OINK!!!".getBytes();
         cooperate();
+        
 
         while(true){
             cooperate();
-            System.out.println("Oink!!! " + number + " pointer = " + pointer);
-            for(int j = 0; j<99; j++){
-                for(int i=0; i<oink.length;i++){
-                    write(pointer+i+(j*UserLandProcess.PAGE_SIZE), oink[i % oink.length]);
-                    cooperate();
-                }
+            
+            for(int i=0; i<oink.length;i++){
+                write(pointer+i+(0*UserLandProcess.PAGE_SIZE), oink[i % oink.length]);
+                cooperate();
             }
+            
+            System.out.println("Oink!!! " + number + " pointer = " + pointer);
             
             OS.sleep(1500);
         }
